56c031982d414e1095c5971e40bdaed45055b3e0
Soohwan Lim <swtee0506@kaist.ac.kr>
2026-05-19 09:40:27 +0900

    StockDAO,DTO 구현

--- src/model/dao/StockDAO.java
+++ src/model/dao/StockDAO.java
@@ -1,19 +1,45 @@
 package model.dao;
 
 import model.dto.StockDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import config.DBConnection;
 
 // [A 담당] Stock 테이블 CRUD
 public class StockDAO {
 
     /**
      * [A] TODO: Stock 테이블 전체 조회
-     *           - try-with-resources 사용
-     *           - PreparedStatement 사용 (SQL Injection 방지)
+     * - try-with-resources 사용
+     * - PreparedStatement 사용 (SQL Injection 방지)
      */
     public List<StockDTO> selectAll() {
         // TODO
-        return null;
+        List<StockDTO> list = new ArrayList<>();
+
+        String sql = "SELECT stock_code, stock_name, current_price FROM Stock ORDER BY stock_name ASC";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                StockDTO dto = new StockDTO();
+                dto.setStockCode(rs.getString("stock_code"));
+                dto.setStockName(rs.getString("stock_name"));
+                dto.setCurrentPrice(rs.getInt("current_price"));
+                list.add(dto);
+            }
+
+        } catch (Exception e) {
+            System.out.println("StockDAO selectAll Error: " + e.getMessage());
+        }
+        return list;
     }
 
     /**
@@ -21,5 +47,17 @@ public class StockDAO {
      */
     public void updatePrice(String stockCode, int price) {
         // TODO
+        String sql = "UPDATE Stock SET current_price = ? WHERE stock_code = ?";
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, price);
+            pstmt.setString(2, stockCode);
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("StockDAO updatePrice Error: " + e.getMessage());
+        }
     }
 }
--- src/model/dto/StockDTO.java
+++ src/model/dto/StockDTO.java
@@ -5,10 +5,45 @@ public class StockDTO {
 
     private String stockCode;
     private String stockName;
-    private int    currentPrice;
+    private int currentPrice;
 
     // [A] TODO: 기본 생성자, 전체 필드 생성자 작성
     // [A] TODO: getter / setter 작성
+    // [A] 기본 생성자
+    public StockDTO() {
+    }
+
+    // [A] 전체 필드 생성자
+    public StockDTO(String stockCode, String stockName, int currentPrice) {
+        this.stockCode = stockCode;
+        this.stockName = stockName;
+        this.currentPrice = currentPrice;
+    }
+
+    // [A] getter / setter
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(int currentPrice) {
+        this.currentPrice = currentPrice;
+    }
 
     // toString()은 JList 표시용 - 완성 예시
     @Override
