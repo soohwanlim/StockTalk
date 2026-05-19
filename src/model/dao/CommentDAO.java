a19f96f771480bd0257d10920d9443e0777cb091
Soohwan Lim <swtee0506@kaist.ac.kr>
2026-05-19 10:27:13 +0900

    CommentDAO 구현

--- src/model/dao/CommentDAO.java
+++ src/model/dao/CommentDAO.java
@@ -1,7 +1,14 @@
 package model.dao;
 
 import model.dto.CommentDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import config.DBConnection;
 
 // [A 담당] Comment 테이블 CRUD
 public class CommentDAO {
@@ -11,7 +18,28 @@ public class CommentDAO {
      */
     public List<CommentDTO> selectByPostId(int postId) {
         // TODO
-        return null;
+        List<CommentDTO> list = new ArrayList<>();
+            String sql = "SELECT comment_id, post_id, reply_content, reply_writer, created_at " +
+                            "FROM Comment WHERE post_id = ? ORDER BY created_at ASC";
+        try (Connection conn = DBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setInt(1, postId);
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    while (rs.next()) {
+                        CommentDTO dto = new CommentDTO();
+                        dto.setCommentId(rs.getInt("comment_id"));
+                        dto.setPostId(rs.getInt("post_id"));
+                        dto.setReplyContent(rs.getString("reply_content"));
+                        dto.setReplyWriter(rs.getString("reply_writer"));
+                        dto.setCreatedAt(rs.getTimestamp("created_at"));
+                        list.add(dto);
+                    }
+                }
+
+        } catch (Exception e) {
+            System.out.println("CommentDAO selectByPostId Error: " + e.getMessage());
+        }
+        return list;
     }
 
     /**
@@ -19,5 +47,16 @@ public class CommentDAO {
      */
     public void insertComment(CommentDTO comment) {
         // TODO
+        String sql = "INSERT INTO Comment (post_id, reply_content, reply_writer) VALUES (?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setInt(1, comment.getPostId());
+                pstmt.setString(2, comment.getReplyContent());
+                pstmt.setString(3, comment.getReplyWriter());
+                pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("CommentDAO insertComment Error: " + e.getMessage());
+        }
     }
 }
