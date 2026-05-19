d2100c275b5bf3bd83c252fb93c7ca52b3a2c2bf
Soohwan Lim <swtee0506@kaist.ac.kr>
2026-05-19 09:44:28 +0900

    DTO 구현 완료

--- src/model/dto/CommentDTO.java
+++ src/model/dto/CommentDTO.java
@@ -5,11 +5,62 @@ import java.sql.Timestamp;
 // [A 담당] 댓글 데이터 전달 객체
 public class CommentDTO {
 
-    private int       commentId;
-    private int       postId;
-    private String    replyContent;
-    private String    replyWriter;
+    private int commentId; // 기본키
+    private int postId; // 외래키
+    private String replyContent;
+    private String replyWriter;
     private Timestamp createdAt;
 
     // [A] TODO: getter / setter 작성
+    public CommentDTO() {
+    }
+
+    public CommentDTO(int commentId, int postId, String replyContent, String replyWriter, Timestamp createdAt) {
+        this.commentId = commentId;
+        this.postId = postId;
+        this.replyContent = replyContent;
+        this.replyWriter = replyWriter;
+        this.createdAt = createdAt;
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getReplyContent() {
+        return replyContent;
+    }
+
+    public void setReplyContent(String replyContent) {
+        this.replyContent = replyContent;
+    }
+
+    public String getReplyWriter() {
+        return replyWriter;
+    }
+
+    public void setReplyWriter(String replyWriter) {
+        this.replyWriter = replyWriter;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
 }
--- src/model/dto/PostDTO.java
+++ src/model/dto/PostDTO.java
@@ -5,14 +5,92 @@ import java.sql.Timestamp;
 // [A 담당] 게시글 데이터 전달 객체
 public class PostDTO {
 
-    private int       postId;
-    private String    stockCode;
-    private String    title;
-    private String    content;
-    private String    writer;
-    private String    postPassword;
+    private int postId; // 기본키
+    private String stockCode; // 외래키
+    private String title;
+    private String content;
+    private String writer;
+    private String postPassword;
     private Timestamp createdAt;
-    private int       replyCount; // LEFT JOIN으로 집계한 댓글 수
+    private int replyCount; // LEFT JOIN으로 집계한 댓글 수
 
     // [A] TODO: getter / setter 작성
+    public PostDTO() {
+    }
+
+    public PostDTO(int postId, String stockCode, String title, String content, String writer, String postPassword,
+            Timestamp createdAt, int replyCount) {
+        this.postId = postId;
+        this.stockCode = stockCode;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.postPassword = postPassword;
+        this.createdAt = createdAt;
+        this.replyCount = replyCount;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getPostPassword() {
+        return postPassword;
+    }
+
+    public void setPostPassword(String postPassword) {
+        this.postPassword = postPassword;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(int replyCount) {
+        this.replyCount = replyCount;
+    }
 }
